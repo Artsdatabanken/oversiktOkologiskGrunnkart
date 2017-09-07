b659d3a4704e365ec187ed25e9b226f2e07e8673
jarle pedersen <jarle.pedersen@norconsult.com>
2017-09-07 12:19:52 +0200

    initial commit

---
+++ Økologisk grunnkart.mm
@@ -0,0 +1,197 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#xd8;kologisk grunnkart" FOLDED="false" ID="ID_333654321" CREATED="1504693132327" MODIFIED="1504699434955" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Databaser" POSITION="right" ID="ID_1160457107" CREATED="1504693154671" MODIFIED="1504701701450" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="-22.499999329447768 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Aktuelle" ID="ID_188169226" CREATED="1504701703631" MODIFIED="1504701757995">
+<node TEXT="Forvaltning" ID="ID_1478848471" CREATED="1504693160968" MODIFIED="1504701282706" LINK="https://www.ntnu.no/wiki/pages/viewpage.action?pageId=112986930&amp;preview=%2F112986930%2F112986931%2F20170721_behovsanalyse_&#xd8;G_NV20170818.docx">
+<node TEXT="R&#xf8;dliste" ID="ID_958628702" CREATED="1504693315005" MODIFIED="1504699644679" LINK="https://www.ntnu.no/wiki/pages/viewpage.action?pageId=110822567"/>
+<node TEXT="Fremmedart (svartliste)" ID="ID_314690351" CREATED="1504693320661" MODIFIED="1504699654955" LINK="https://www.ntnu.no/wiki/display/artsdatabanken/Fremmedartsbase"/>
+<node TEXT="Artskart (observasjoner)" ID="ID_1089444290" CREATED="1504701005418" MODIFIED="1504701282706" HGAP_QUANTITY="11.750000067055222 pt" VSHIFT_QUANTITY="0.7499999776482582 pt"/>
+<node TEXT="Artsnavnebasen" ID="ID_562140495" CREATED="1504699109753" MODIFIED="1504699663660" LINK="https://www.ntnu.no/wiki/display/artsdatabanken/Artsnavnebasen"/>
+</node>
+<node TEXT="Distribusjon" ID="ID_74333976" CREATED="1504693177295" MODIFIED="1504693203193">
+<node TEXT="Postgresql vs SQL Server" ID="ID_1533309189" CREATED="1504701913828" MODIFIED="1504701925707"/>
+</node>
+<node TEXT="NiNCore" ID="ID_407210324" CREATED="1504698914802" MODIFIED="1504699988208" LINK="http://folk.ntnu.no/bjornrep/ADB_Exchange.zip"/>
+<node TEXT="RavenDB (?)" ID="ID_310628710" CREATED="1504699197953" MODIFIED="1504701393536"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Brukes for lagring av (sm&#229;) dokumenter
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Nice2Have" ID="ID_1688910674" CREATED="1504701764842" MODIFIED="1504701884043">
+<node TEXT="Egenskapsbank" ID="ID_1584329645" CREATED="1504693223528" MODIFIED="1504701500414" LINK="https://www.ntnu.no/wiki/pages/viewpage.action?pageId=112986930&amp;preview=%2F112986930%2F112986931%2F20170721_behovsanalyse_&#xd8;G_NV20170818.docx"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Datakilde for sp&#248;rringer p&#229; tvers av arter. Kandidat for graph-database og rdf. Ontologier m&#229; finnes/opprettes.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Databanken" ID="ID_1833546027" CREATED="1504699160057" MODIFIED="1504699670905" LINK="https://www.ntnu.no/wiki/display/artsdatabanken/Databanken"/>
+</node>
+</node>
+<node TEXT="Analyse" POSITION="right" ID="ID_1852837601" CREATED="1504693343461" MODIFIED="1504693345622">
+<edge COLOR="#00ff00"/>
+<node TEXT="R&#xf8;dliste" ID="ID_828063010" CREATED="1504698940954" MODIFIED="1504698946049"/>
+<node TEXT="PCA (?)" ID="ID_1421714932" CREATED="1504702070891" MODIFIED="1504702253497" LINK="https://stats.stackexchange.com/a/140579"/>
+</node>
+<node TEXT="Import" POSITION="left" ID="ID_958565679" CREATED="1504693900272" MODIFIED="1504693905513">
+<edge COLOR="#7c0000"/>
+<node TEXT="Datakilder" ID="ID_903403074" CREATED="1504693910928" MODIFIED="1504700075868">
+<node TEXT="H&#xf8;ydedata" ID="ID_84687076" CREATED="1504699455674" MODIFIED="1504699461915"/>
+<node TEXT="SSB (?)" ID="ID_1462690511" CREATED="1504699476891" MODIFIED="1504699486507"/>
+<node TEXT="Milj&#xf8;variabeldata" ID="ID_145513510" CREATED="1504700062242" MODIFIED="1504700071304" LINK="http://folk.ntnu.no/oyvibone/">
+<node TEXT="NGU" ID="ID_1453550653" CREATED="1504699474004" MODIFIED="1504700075867" HGAP_QUANTITY="43.2499991282821 pt" VSHIFT_QUANTITY="8.999999731779106 pt">
+<node TEXT="Kalk" ID="ID_561414561" CREATED="1504699493387" MODIFIED="1504699506676"/>
+<node TEXT="Ultramafisk" ID="ID_491911586" CREATED="1504699508027" MODIFIED="1504699511491"/>
+</node>
+<node TEXT="NiBIO" ID="ID_1278926794" CREATED="1504778574522" MODIFIED="1504778580665">
+<node TEXT="Vannmetning" ID="ID_1723664701" CREATED="1504778582840" MODIFIED="1504778588499"/>
+</node>
+</node>
+<node TEXT="Naturomr&#xe5;detype" FOLDED="true" ID="ID_650492100" CREATED="1504778965569" MODIFIED="1504778992824"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Samme som objekttype
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Natursystemdata" ID="ID_1542313816" CREATED="1504778520660" MODIFIED="1504778525175">
+<node TEXT="Milj&#xf8;direktoratet" ID="ID_463717464" CREATED="1504699462595" MODIFIED="1504778534014"/>
+</node>
+<node TEXT="Landskapstyper" ID="ID_926982344" CREATED="1504778553578" MODIFIED="1504778558274">
+<node TEXT="NINA" ID="ID_1622358563" CREATED="1504778560521" MODIFIED="1504778564874"/>
+</node>
+</node>
+</node>
+<node TEXT="Importrutiner" ID="ID_512545044" CREATED="1504693937120" MODIFIED="1504693940689"/>
+<node TEXT="Darwin core" FOLDED="true" ID="ID_59798589" CREATED="1504699833042" MODIFIED="1504702361380" LINK="https://www.ntnu.no/wiki/pages/viewpage.action?pageId=112986930&amp;preview=%2F112986930%2F112986931%2F20170721_behovsanalyse_&#xd8;G_NV20170818.docx"><richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Brukes for innsamling for Artskart
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="GBIF" ID="ID_423399980" CREATED="1504699844952" MODIFIED="1504699886016" LINK="https://www.gbif.org"/>
+</node>
+<node TEXT="GDAL/Ogr" ID="ID_83844612" CREATED="1504699895672" MODIFIED="1504699930646" LINK="http://www.gdal.org"/>
+</node>
+<node TEXT="Innsyn" POSITION="right" ID="ID_1519249296" CREATED="1504700664182" MODIFIED="1504700695944">
+<edge COLOR="#007c00"/>
+<node TEXT="Klienter" ID="ID_945601456" CREATED="1504693248047" MODIFIED="1504700698870">
+<node TEXT="Naturtypekart" ID="ID_1861503893" CREATED="1504700279101" MODIFIED="1504700654870" LINK="https://www.ntnu.no/wiki/display/artsdatabanken/Naturtypekart" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="20.999999374151248 pt"/>
+<node TEXT="Arter p&#xe5; nett" ID="ID_1432872677" CREATED="1504700315708" MODIFIED="1504700321235" LINK="https://www.ntnu.no/wiki/pages/viewpage.action?pageId=110822573"/>
+<node TEXT="Artskart 2.0" ID="ID_1622734847" CREATED="1504700328718" MODIFIED="1504700333947" LINK="https://www.ntnu.no/wiki/display/artsdatabanken/Artskart+2.0"/>
+<node TEXT="Artsobservasjoner" ID="ID_32381233" CREATED="1504700359581" MODIFIED="1504700365587" LINK="https://www.ntnu.no/wiki/display/artsdatabanken/Artsobservasjoner"/>
+<node TEXT="Artsportalen (?)" ID="ID_906089939" CREATED="1504700376006" MODIFIED="1504700383651" LINK="https://www.ntnu.no/wiki/display/artsdatabanken/Artsportalen"/>
+<node TEXT="Datakatalog" ID="ID_64141591" CREATED="1504693360750" MODIFIED="1504700720065" LINK="https://www.ntnu.no/wiki/pages/viewpage.action?pageId=112986930&amp;preview=%2F112986930%2F112986931%2F20170721_behovsanalyse_&#xd8;G_NV20170818.docx">
+<node TEXT="Geonorge (?)" ID="ID_1579413743" CREATED="1504693799913" MODIFIED="1504693804697"/>
+<node TEXT="Webside" ID="ID_1147571298" CREATED="1504693809250" MODIFIED="1504693811690"/>
+</node>
+</node>
+<node TEXT="Nedlasting" ID="ID_1364934858" CREATED="1504700245084" MODIFIED="1504700707638">
+<node TEXT="Filbasert" ID="ID_550094107" CREATED="1504693262087" MODIFIED="1504701489973"/>
+<node TEXT="WFS" ID="ID_69448262" CREATED="1504693258718" MODIFIED="1504693260416"/>
+<node TEXT="WMS" ID="ID_1246756460" CREATED="1504693252783" MODIFIED="1504693257609"/>
+</node>
+</node>
+<node TEXT="Datamodeller" POSITION="left" ID="ID_143388520" CREATED="1504700852163" MODIFIED="1504700855413">
+<edge COLOR="#7c007c"/>
+<node TEXT="Natur i Norge" ID="ID_175811441" CREATED="1504700856300" MODIFIED="1504700864924"/>
+<node TEXT="R&#xf8;dlistearter/svartlistearter" ID="ID_496948979" CREATED="1504701447545" MODIFIED="1504701461727" LINK="http://skjema.geonorge.no/SOSI/produktspesifikasjon/Rodlistearter-Svartelistearter/20170301/rodlistearter_svartelistearter.xsd"/>
+</node>
+</node>
+</map>
